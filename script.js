ff26a7ec38b50dde0d1eada86523896a9aab3df5
Emil Alex <97882056+emil0519@users.noreply.github.com>
2022-06-02 08:33:42 +0000

    nearly done

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-const navToggle = document.querySelector('.header-button');
-const navLinks= document.querySelector('.nav-link')
+//Nav list//
+navToggle = document.querySelector('.header-button');
+const navLinks = document.querySelectorAll('.nav-link')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
-// document.querySelector('.my-element').addEventListener('click'
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
 
-// const removeOpen (navLinks)=>{
-//   for (link of links){
-//     link.addEventListener('click', () => {
-//       document.body.classList.toggle('nav-open');
-//   }
-// }
-// };
+//Advocacy list//
 
-// navLinks.forEach(link => {
-//     link.addEventListener('click', () => {
-//       document.body.classList.toggle('nav-open');
-//     })
-// })
+const btnCreate = document.querySelector('.btn-main');
+const btnToggle = document.querySelector('.btn-toggle');
+const btnRemove = document.querySelector('.btn-remove');
 
+btnCreate.addEventListener('click', () => {												 
+  const input = document.querySelector('.input-main');
+	const list = document.querySelector('.ul');
 
+	list.insertAdjacentHTML(
+		'afterbegin',
+		`<li class="a-li">${input.value}</li>`
+	);
+  input.value = '';
+});
 
+btnToggle.addEventListener('click', () => {
+  const listContainer = document.querySelector('.list-container');
 
-//因為網頁設計與功能所限，未能完全地展現我的Javascript進度，我誠摯邀請您到git中的demo.js中，瀏覽我在其他課程中的Javascript實作//
+	if (listContainer.style.display === 'none') {
+		btnToggle.textContent = 'Hide List';
+		listContainer.removeAttribute('style');
+	} else {
+		btnToggle.textContent = 'Show List';
+		listContainer.style.display = 'none'; 	
+	}		
+});
+
+btnRemove.addEventListener('click', () => {
+	const lastItem = document.querySelector('.a-li:first-child');												lastItem.remove(); 
+});
+
+//因為網頁設計與功能所限，未能完全地展現我的Javascript進度，此部分的Javascript也有參考不同課程中的作業。我誠摯邀請您到git中的demo.js，瀏覽我在其他課程中的Javascript實作//
