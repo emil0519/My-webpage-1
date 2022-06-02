7a5795651410308bb42fc3650879e47e42c18571
Emil Alex <97882056+emil0519@users.noreply.github.com>
2022-06-02 04:00:35 +0000

    nav button works normally

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const navToggle = document.querySelector('.header-button');
+const navLinks= document.querySelector('.nav-link')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+// document.querySelector('.my-element').addEventListener('click'
+
+// const removeOpen (navLinks)=>{
+//   for (link of links){
+//     link.addEventListener('click', () => {
+//       document.body.classList.toggle('nav-open');
+//   }
+// }
+// };
+
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//       document.body.classList.toggle('nav-open');
+//     })
+// })
+
+
+
+
+//因為網頁設計與功能所限，未能完全地展現我的Javascript進度，我誠摯邀請您到git中的demo.js中，瀏覽我在其他課程中的Javascript實作//
